174bfb0b46ed8abe1b6573975d3a922d30b42a4b
Fred Jonsson <fjnsson@gmail.com>
2014-03-05 14:55:38 +0400

    breakthrough, haha

--- src/main/scala/com/github/freddd/board/Board.scala
+++ src/main/scala/com/github/freddd/board/Board.scala
@@ -6,22 +6,95 @@ import Symbol._
  * Created by fred on 3/3/14.
  */
 class Board {
-  var board = Array.ofDim[Option[Symbol]](3,3)
+  var board = Array.tabulate[Option[Symbol]](3,3)((x, y) => None) //Array.ofDim[Option[Symbol]](3,3)
 
-  def move(symbol: Symbol, x: Int, y: Int): Boolean = validMove(x,y) match {
-    case true => board(x)(y) = Some(symbol); true
-    case _ => false
+  def move(symbol: Symbol, x: Int, y: Int): Unit = validMove(x,y) match {
+    case true => board(x)(y) = Some(symbol)
   }
-  def validMove(x: Int, y: Int): Boolean = (x <= 2 && y <= 2) && board(x)(y) == null
-  def reset(): Unit = {board = Array.ofDim[Option[Symbol]](3,3)}
-  def completed: Boolean = board forall { p => p forall { p2 => !p2.isEmpty} }
+
+  /**
+   * Convenience method to make sure that the move is valid (and not out of bounds or trying to populate a square that is already populated
+   * @param x
+   * @param y
+   * @return
+   */
+  def validMove(x: Int, y: Int): Boolean = (x <= 2 && y <= 2) && !board(x)(y).isDefined
+
+  /**
+   * Convenience method to reset the board to play another game
+   */
+  def reset(): Unit = {board = Array.tabulate[Option[Symbol]](3,3)((x, y) => None)}
+
+  /**
+   * Convenience method to know whether the game is completed or not
+   * @return
+   */
+  def completed: Boolean = board forall { p => p forall { p2 => p2.isDefined} }
+
+  /**
+   * Convenience method to know whether or not the game is a draw
+   * @return
+   */
   def draw: Boolean = {
     completed match {
       case true => !win(Symbol.O) && !win(Symbol.X)
       case _ => false
     }
   }
-  def win(symbol: Symbol): Boolean = {
-    ???
+
+  /**
+   * Convenience method to know if the game has been won by either player
+   * @param symbol
+   * @return
+   */
+  def win(symbol: Symbol): Boolean = horizontal(symbol) || vertical(symbol) || diagonal(symbol)
+
+  /**
+   * Used by <method>win</method> to assess if either player has 3 symbols in a row horizontally
+   * @param symbol
+   * @return
+   */
+  private def horizontal(symbol: Symbol): Boolean = {
+    board exists ( row => row forall (col => col.isDefined && col.get.equals(symbol)))
+  }
+
+  /**
+   * Used by <method>win</method> to assess if either player has 3 symbols in a row vertically
+   * @param symbol
+   * @return
+   */
+  private def vertical(symbol: Symbol): Boolean = {
+    //board.flatten.zipWithIndex.filter(p => p._2 % 3 == 0).forall(p => p._1.isDefined && p._1.get.equals(symbol))
+
+    val all: Array[Option[Symbol]] = board.flatten
+    validate(symbol, all(0)) && validate(symbol, all(3)) && validate(symbol, all(6)) ||
+    validate(symbol, all(1)) && validate(symbol, all(4)) && validate(symbol, all(7)) ||
+    validate(symbol, all(2)) && validate(symbol, all(5)) && validate(symbol, all(8))
+  }
+
+  private def validate(symbol: Symbol, square: Option[Symbol]): Boolean = {
+    square match {
+      case Some(_) => square.get.equals(symbol)
+      case _ => false
+    }
+  }
+
+  /**
+   * Used by <method>win</method> to assess if either player has 3 symbols in a row diagonally
+   * @param symbol
+   * @return
+   */
+  private def diagonal(symbol: Symbol): Boolean = {
+    val all: Array[Option[Symbol]] = board.flatten
+    validate(symbol, all(0)) && validate(symbol, all(4)) && validate(symbol, all(8)) ||
+    validate(symbol, all(2)) && validate(symbol, all(4)) && validate(symbol, all(6))
+  }
+
+  /**
+   * Returning the current state of the board
+   * @return
+   */
+  override def toString: String = {
+    board.deep.mkString("\n")
   }
 }
--- src/test/scala/com/github/freddd/board/BoardSpec.scala
+++ src/test/scala/com/github/freddd/board/BoardSpec.scala
@@ -1,25 +1,27 @@
 package com.github.freddd.board
 
 import org.specs2.mutable._
+import Symbol._
 
 /**
  * Created by fred on 3/3/14.
  */
 class BoardSpec extends Specification {
-  var board: Board = new Board()
-  var innerBoard: Array[Array[Option[Symbol.Symbol]]] = board.board
 
   "innerBoard" should {
+    val board: Board = new Board()
+
     "columns should be of length 3" in {
-      innerBoard.length mustEqual 3
+      board.board.length mustEqual 3
     }
 
     "rows should be of length 3" in {
-      innerBoard(0).length mustEqual 3
+      board.board(0).length mustEqual 3
     }
   }
 
   "board.validMove" should {
+    val board: Board = new Board()
 
     "Fail if the move is outside of bounds" in {
       board.validMove(3,3) must beFalse
@@ -35,14 +37,50 @@ class BoardSpec extends Specification {
   }
 
   "board.completed" should {
+    val board: Board = new Board()
+
     "Fail any square is empty" in {
       board.completed must beFalse
     }
 
-    /*
+
     "Succeed if no square is empty" in {
-      board.completed must beTrue
+      val b: Board = new Board()
+      b.move(Symbol.X,0,0)
+      b.move(Symbol.X,0,1)
+      b.move(Symbol.X,0,2)
+      b.move(Symbol.X,1,0)
+      b.move(Symbol.X,1,1)
+      b.move(Symbol.X,1,2)
+      b.move(Symbol.X,2,0)
+      b.move(Symbol.X,2,1)
+      b.move(Symbol.X,2,2)
+
+      b.completed must beTrue
+
+      b.reset()
     }
-    */
+
   }
+
+
+  "board.move" should {
+    "Succeed if there is a symbol in populated square" in {
+      val b: Board = new Board()
+      b.move(Symbol.X,0,0)
+      b.board(0)(0).equals(Some(Symbol.X)) must beTrue
+    }
+  }
+
+
+  "board.reset" should {
+    "Succeed if there is no square populated" in {
+      val b: Board = new Board()
+      b.move(Symbol.X,0,0)
+      b.reset()
+      b.board(0)(0).isDefined must beFalse
+    }
+  }
+
+
 }
